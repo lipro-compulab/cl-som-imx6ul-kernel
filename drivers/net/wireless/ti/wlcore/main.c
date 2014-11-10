bcb3596a3196c4b44e43635a56949a9b6587f73d
Radek Dostal <radek.dostal@streamunlimited.com>
2014-11-10 11:26:19 +0100

    Revert "wlcore: always register dummy hardirq"
    This reverts commit f2cede49ae7b9f51a6fe97ada16c27e4d03d05a3.

--- drivers/net/wireless/ti/wlcore/main.c
+++ drivers/net/wireless/ti/wlcore/main.c
@@ -6094,11 +6094,6 @@ static const struct wiphy_wowlan_support wlcore_wowlan_support = {
 };
 #endif
 
-static irqreturn_t wlcore_hardirq(int irq, void *cookie)
-{
-	return IRQ_WAKE_THREAD;
-}
-
 static void wlcore_nvs_cb(const struct firmware *fw, void *context)
 {
 	struct wl1271 *wl = context;
@@ -6107,7 +6102,6 @@ static void wlcore_nvs_cb(const struct firmware *fw, void *context)
 	struct wl12xx_platform_data *pdata = pdev_data->pdata;
 	unsigned long irqflags;
 	int ret;
-	irq_handler_t hardirq_fn = NULL;
 
 	if (fw) {
 		wl->nvs = kmemdup(fw->data, fw->size, GFP_KERNEL);
@@ -6136,14 +6130,12 @@ static void wlcore_nvs_cb(const struct firmware *fw, void *context)
 	wl->platform_quirks = pdata->platform_quirks;
 	wl->if_ops = pdev_data->if_ops;
 
-	if (wl->platform_quirks & WL12XX_PLATFORM_QUIRK_EDGE_IRQ) {
+	if (wl->platform_quirks & WL12XX_PLATFORM_QUIRK_EDGE_IRQ)
 		irqflags = IRQF_TRIGGER_RISING;
-		hardirq_fn = wlcore_hardirq;
-	} else {
+	else
 		irqflags = IRQF_TRIGGER_HIGH | IRQF_ONESHOT;
-	}
 
-	ret = request_threaded_irq(wl->irq, hardirq_fn, wlcore_irq,
+	ret = request_threaded_irq(wl->irq, NULL, wlcore_irq,
 				   irqflags, pdev->name, wl);
 	if (ret < 0) {
 		wl1271_error("request_irq() failed: %d", ret);
